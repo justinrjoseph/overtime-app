b32af8b5cd3450e5ebb69d95d76fd9ae2c6bd716
nsiderwolverine <justin.joseph0@yahoo.com>
2016-09-12 22:30:22 +0000

    Implement post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,29 @@ class PostsController < ApplicationController
   def index
     
   end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      redirect_to @post
+    else
+      flash[:error] = 'There was a problem creating your post.'
+      render :new
+    end
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  private
+  
+    def post_params
+      params.require(:post).permit(:date, :rationale)
+    end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -14,4 +14,24 @@ describe 'navigation' do
       expect(page).to have_content 'Posts'
     end
   end
+  
+  describe 'creation' do
+    before do
+      visit new_post_path
+    end
+    
+    it 'has a new form that can be reached' do
+      expect(page.status_code).to eq 200
+    end
+    
+    it 'can be created using new form' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'Some rationale.'
+      
+      click_on 'Save'
+      
+      expect(page).to have_content Date.today.strftime('%m/%d/%Y')
+      expect(page).to have_content 'Some rationale.'
+    end
+  end
 end

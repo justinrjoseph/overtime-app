eb04e654d77c998e32b8f1c4074a2b207f7ceb88
nsiderwolverine <justin.joseph0@yahoo.com>
2016-09-27 21:41:53 +0000

    Add AdminUser seed and modify post dashboard setup

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -13,7 +13,8 @@ class PostDashboard < Administrate::BaseDashboard
     date: Field::DateTime.with_options(searchable: false),
     rationale: Field::Text.with_options(searchable: true),
     created_at: Field::DateTime.with_options(searchable: false),
-    updated_at: Field::DateTime.with_options(searchable: false)
+    updated_at: Field::DateTime.with_options(searchable: false),
+    status: Field::Text.with_options(searchable: true)
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -23,7 +24,7 @@ class PostDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
     :user,
-    :id,
+    :status,
     :date,
     :rationale,
   ].freeze
@@ -32,6 +33,7 @@ class PostDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :user,
+    :status,
     :id,
     :date,
     :rationale,
@@ -45,7 +47,7 @@ class PostDashboard < Administrate::BaseDashboard
   FORM_ATTRIBUTES = [
     :user,
     :date,
-    :rationale,
+    :rationale
   ].freeze
 
   # Overwrite this method to customize how posts are displayed
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 @user = User.create(email: 'test@example.com', password: 'asdf1234', password_confirmation: 'asdf1234', first_name: 'John', last_name: 'Doe')
 User.create(email: 'justin@example.com', password: 'asdf1234', password_confirmation: 'asdf1234', first_name: 'Justin', last_name: 'Joseph')
+AdminUser.create(email: 'admin@example.com', password: 'asdf1234', password_confirmation: 'asdf1234', first_name: 'Admin', last_name: 'User')
 
-puts "Two users created."
+puts "Two users, and one AdminUser created."
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "Rationale for post #{post + 1}.", user: @user)

8270682981f902f9ecd47990b71c84ca08604f12
nsiderwolverine <justin.joseph0@yahoo.com>
2016-10-01 23:21:14 +0000

    Add content to audit logs index template, and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :audit_logs
+  resources :audit_logs, except: [:new, :edit, :destroy]
+  
   namespace :admin do
     resources :users
-resources :posts
-resources :admin_users
-
+    resources :posts
+    resources :admin_users
+    
     root to: "users#index"
   end
 
---
+++ spec/features/audit_log_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'AuditLog feature' do
+  let(:user) { FactoryGirl.create(:admin_user) }
+  
+  before do
+    FactoryGirl.create(:audit_log)
+    login_as user, scope: :user
+  end
+  
+  describe 'index' do
+    it 'has an index page that can be reached' do
+      visit audit_logs_path
+      expect(page.status_code).to eq 200
+    end
+    
+    it 'renders audit log content' do
+      visit audit_logs_path
+      expect(page).to have_content AuditLog.last.user.full_name
+    end
+    
+    it 'can not be accessed by non-admins' do
+      
+    end
+  end
+end

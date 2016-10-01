0bff3f2223f8964e7e8bf4bbd8b3d6618dfbe829
nsiderwolverine <justin.joseph0@yahoo.com>
2016-10-01 22:35:16 +0000

    Add validations for audit logs

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,15 @@
 class AuditLog < ActiveRecord::Base
   belongs_to :user
+  
+  validates :user, presence: true
+  validates :status, presence: true
+  validates :start_date, presence: true
+  
+  after_initialize :set_defaults
+  
+  private
+  
+    def set_defaults
+      self.start_date ||= Date.today - 6.days
+    end
 end
--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe AuditLog, type: :model do
+  before do
+    @audit_log = FactoryGirl.create(:audit_log)
+  end
   
+  describe 'creation' do
+    it 'can be created' do
+      expect(@audit_log).to be_valid
+    end
+  end
+  
+  describe 'validations' do
+    it 'should have a user assocation' do
+      @audit_log.user = nil
+      expect(@audit_log).to_not be_valid
+    end
+    
+    it 'should always have a status' do
+      @audit_log.status = nil
+      expect(@audit_log).to_not be_valid
+    end
+    
+    it 'should have a start_date' do
+      @audit_log.start_date = nil
+      expect(@audit_log).to_not be_valid
+    end
+    
+    it 'should have a start_date six days prior' do
+      new_audit_log = AuditLog.create(user: User.last)
+      expect(new_audit_log.start_date).to eq Date.today - 6.days
+    end
+  end
 end

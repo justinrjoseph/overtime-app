e874adcc344f83ac53540199df895ae972fa7e93
nsiderwolverine <justin.joseph0@yahoo.com>
2016-10-02 20:21:36 +0000

    Implement callback to update audit log when post created

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,13 @@ class Post < ActiveRecord::Base
   validates :overtime_request, presence: true, numericality: { greater_than: 0.0 }
   
   scope :those_by, ->(user) { where(user_id: user.id) }
+  
+  after_save :update_audit_log
+  
+  private
+  
+    def update_audit_log
+      audit_log = AuditLog.where(user: self.user, start_date: (self.date - 7.days..self.date)).last
+      audit_log.confirmed!
+    end
 end
